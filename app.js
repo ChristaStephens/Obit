0f6889c6f3b57db77b9e7aba7e2eebb15151cc47
Christa Stephens <christastephens@quickenloans.com>
2019-10-15 11:15:42 -0400

    finished building template of website

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ function splitScroll() {
     //duration is in pixels and trigger is what will trigger our scrolling
     //we want the about title to trigger our scroll
     new ScrollMagic.Scene({
-        duration: 500,
-        triggerElement: '.about-title'
+        duration: '200%',
+        triggerElement: '.about-title',
+        //sets the start of the scroll effect to start in the middle
+        triggerHook: 0
     })
+    //as soon as we hit the about title the title will pop up
+    .setPin('.about-title')
+    //adds triggers to page and show's where it will start scrolling effect
         .addIndicators()
+        //starts every thing
         .addTo(controller);
 
 }
